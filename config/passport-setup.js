61bc7c9dc4eb2de1307d9559b1b60da19bf1b62d
jesvy <visayait111@gmail.com>
2020-04-09 04:40:10 +0800

    second commit

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,13 +4,16 @@ const keys = require('./keys');
 
 passport.use(
     new GoogleStrategy({
-        // options for the google strat
+        
         callbackURL: '/auth/google/callback',
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
     }, (accessToken, refreshToken, profile, done)=>{
-        // passport callback function
+     
         console.log('##########################');
         console.log(profile);
+        console.log(profile.id)
+        console.log(profile.displayName)
+        console.log(profile.photos[0].value)
     })
 );
